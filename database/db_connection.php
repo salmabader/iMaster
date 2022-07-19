0d997c787b2e15710d91d1d21a67c91054d8266f
salmabader <salma-bader-15@hotmail.com>
2022-07-19 11:08:16 +0300

    edit db info

--- database/db_connection.php
+++ database/db_connection.php
@@ -3,9 +3,9 @@
 function OpenCon()
 {
     $dbhost = "localhost";
-    $dbuser = "bidchain_project";
-    $dbpass = "lrssz17to22";
-    $db = "bidchain";
+    $dbuser = "root";
+    $dbpass = "";
+    $db = "student_registration";
     $conn = new mysqli($dbhost, $dbuser, $dbpass, $db) or die("Connect failed: %s\n" . $conn->error);
     if (!$conn) {
         exit();
