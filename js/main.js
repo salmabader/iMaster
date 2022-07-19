aa6056251b6d76a1a26c7ae3d0ddada9fcca8c16
salmabader <salma-bader-15@hotmail.com>
2022-07-19 13:06:04 +0300

    check password

---
+++ js/main.js
@@ -0,0 +1,73 @@
+// ----------- Signup Script -----------
+let inputPassword = document.getElementById("signup_password")
+let passlength = document.getElementById("passLength")
+let capitalL = document.getElementById("passCL")
+let smallL = document.getElementById("passSL")
+let specialChar = document.getElementById("passSC")
+inputPassword.addEventListener("keyup", function () {
+    // check password length
+    if (inputPassword.value.length < 8) {
+        passlength.innerHTML = `<svg class="text-red-500 inline h-5" xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+        <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd" />
+      </svg>`
+    }
+    else {
+        passlength.innerHTML = `<svg class="text-green-500 inline h-5" xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+        <path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd" />
+      </svg>`
+    }
+
+    // check if there is a capital letter
+    if (isUpper(inputPassword.value)) {
+        capitalL.innerHTML = `<svg class="text-green-500 inline h-5" xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+        <path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd" />
+      </svg>`
+    }
+    else {
+        capitalL.innerHTML = `<svg class="text-red-500 inline h-5" xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+        <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd" />
+      </svg>`
+    }
+
+    // check if there is a small letter
+    if (isLower(inputPassword.value)) {
+        smallL.innerHTML = `<svg class="text-green-500 inline h-5" xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+        <path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd" />
+      </svg>`
+    }
+    else {
+        smallL.innerHTML = `<svg class="text-red-500 inline h-5" xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+        <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd" />
+      </svg>`
+    }
+
+    // check if there is a special character
+    if (isSpecialChar(inputPassword.value)) {
+        specialChar.innerHTML = `<svg class="text-green-500 inline h-5" xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+        <path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd" />
+      </svg>`
+    }
+    else {
+        specialChar.innerHTML = `<svg class="text-red-500 inline h-5" xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+        <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd" />
+      </svg>`
+    }
+
+    // clear the icon when the password empty
+    if (inputPassword.value.length == 0) {
+        passlength.innerHTML = ""
+        capitalL.innerHTML = ""
+        smallL.innerHTML = ""
+        specialChar.innerHTML = ""
+    }
+})
+function isUpper(str) {
+    return /[A-Z]/.test(str)
+}
+function isLower(str) {
+    return /[a-z]/.test(str)
+}
+function isSpecialChar(str) {
+    return /[!@#$%^&*()_+\-=\[\]{};:"\\|,.<>\/?~]/.test(str)
+}
+// ----------- End of Signup Script -----------
