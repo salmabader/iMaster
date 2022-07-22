37b9bcbfa234b0678a5f507a0c29affaa412b694
salmabader <salma-bader-15@hotmail.com>
2022-07-22 20:21:40 +0300

    add loading button

--- restorePassword.php
+++ restorePassword.php
@@ -134,7 +134,7 @@ if (isset($_POST['sendCode'])) {
             <span class="sr-only">Check icon</span>
         </div>
         <div class="ml-3 text-sm font-normal" id="feedback"><?php if (isset($feedback)) echo htmlspecialchars($feedback) ?></div>
-        <button type="button" class="ml-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700" data-dismiss-target="#toast-success" aria-label="Close">
+        <button type="button" class="ml-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 p-1.5 hover:bg-gray-100 inline-flex h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700" data-dismiss-target="#toast-success" aria-label="Close">
             <span class="sr-only">Close</span>
             <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                 <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path>
@@ -182,7 +182,7 @@ if (isset($_POST['sendCode'])) {
                 </div>
                 <!-- button -->
                 <div class="flex flex-col items-center">
-                    <button type="submit" name="sendCode" id="sendCode" class="mt-10 bg-blue-500 text-white px-14 py-3 rounded-full shadow-md font-semibold hover:bg-blue-600 duration-100 ease-in-out disabled:opacity-60 disabled:pointer-events-none" data-tooltip-target="tooltip-hover" data-tooltip-trigger="hover" data-tooltip-placement="top" disabled>Send a code</button>
+                    <button type="submit" name="sendCode" id="sendCode" class="flex justify-center items-center mt-10 bg-blue-500 text-white px-14 py-3 rounded-full shadow-md font-semibold hover:bg-blue-600 duration-100 ease-in-out disabled:opacity-60 disabled:pointer-events-none" data-tooltip-target="tooltip-hover" data-tooltip-trigger="hover" data-tooltip-placement="top" disabled>Send a code</button>
 
                     <p class="text-xs mt-4">New user? <span class=" text-blue-600 hover:underline"><a href="createStudentAccount.php">create account</a></span></p>
                 </div>
