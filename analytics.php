012752b51ca9a92a6bd7f10205b30c6123a5500d
salmabader <salma-bader-15@hotmail.com>
2022-07-29 14:11:12 +0300

    add graphs

--- analytics.php
+++ analytics.php
@@ -1,16 +1,29 @@
 <?php
 session_start();
 require 'database/db_connection.php';
+$con = OpenCon();
 // if there is a session but user is not admin
-// if (isset($_SESSION['type'])) {
-// 	$privilage = $_SESSION['type'];
-// 	if ($privilage != "admin") {
-// 		header('Location: index.php');
-// 	}
-// } else { // if there is no session
-// 	header('Location: index.php');
-// 	exit();
-// }
+if (isset($_SESSION['type'])) {
+	$privilage = $_SESSION['type'];
+	if ($privilage != "admin") {
+		header('Location: index.php');
+	}
+} else { // if there is no session
+	header('Location: index.php');
+	exit();
+}
+if (isset($_POST['saveChangesBtn'])) {
+	$fName = filter_input(INPUT_POST, 'fname');
+	$lName = filter_input(INPUT_POST, 'lname');
+	// need to catch the photo
+	// $photo = filter_input(INPUT_POST, 'photo');
+	// $password = filter_input(INPUT_POST, 'password');
+	$query = "UPDATE $privilage SET FName = ?, LName = ? WHERE username ='" . $_SESSION['username'] . "'";
+	$statement = mysqli_stmt_init($con);
+	mysqli_stmt_prepare($statement, $query);
+	mysqli_stmt_bind_param($statement, "ss", $fName, $lName);
+	mysqli_stmt_execute($statement);
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -43,15 +56,38 @@ require 'database/db_connection.php';
 		.scrollbar::-webkit-scrollbar-thumb:hover {
 			background: rgb(193, 193, 193);
 		}
+
+		#dropdownNotification {
+			left: -1rem !important;
+		}
+
+		#dropdownNotification::before {
+			content: "";
+			width: 13px;
+			height: 13px;
+			background: #f9fafb;
+			position: absolute;
+			top: -7px;
+			right: 35px;
+			transform: rotate(45deg);
+			border-top: 1px solid #f3f4f6;
+			border-left: 1px solid #f3f4f6;
+		}
 	</style>
+	<!-- prevent resubmission when refresh the page -->
+	<script>
+		if (window.history.replaceState) {
+			window.history.replaceState(null, null, window.location.href);
+		}
+	</script>
 </head>
 
 <body class="h-screen w-screen overflow-hidden">
 	<?php
 	// retreive admin info:
-	// $username = $_SESSION['username'];
-	// $query = "SELECT * FROM admin WHERE username = '$username'";
-	// compelete here
+	$query = "SELECT * FROM admin WHERE username ='" . $_SESSION['username'] . "'";
+	$result = mysqli_query($con, $query);
+	$admin = mysqli_fetch_assoc($result);
 	?>
 	<main class="flex w-full h-full">
 		<!-- left side -->
@@ -110,22 +146,27 @@ require 'database/db_connection.php';
 					<!-- profile -->
 					<div class="w-2/3 flex items-center mb-2">
 						<!-- personal photo -->
-						<div class="md:w-1/6 sm:w-1/4 h-full flex md:justify-end sm:justify-start justify-center w-1/2">
-							<a href="#profile">
-								<img src="images/default_user_img.jpg" class="h-full rounded-full ml-2 border-[1px] border-gray-400">
-							</a>
+						<div class="md:w-1/6 sm:w-1/4 h-full flex md:justify-end sm:justify-start justify-center w-1/2 ">
+							<button data-modal-toggle="profile-modal" class="relative">
+								<img src="images/default_user_img.jpg" class="h-full rounded-full ml-2 border-[1px] border-gray-400 hover:shadow-md hover:shadow-amber-300 shadow transition-all ease-in-out duration-200">
+								<div class="absolute bottom-3 -right-1 bg-amber-200 p-[2px] rounded-full border-[1px] border-gray-400">
+									<svg xmlns="http://www.w3.org/2000/svg" class="h-4" viewBox="0 0 20 20" fill="currentColor">
+										<path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z" />
+									</svg>
+								</div>
+							</button>
 						</div>
 						<!-- greeting -->
 						<div class="md:w-5/6 flex flex-col h-full justify-center sm:ml-3 sm:w-3/4 w-1/2">
-							<div class="w-full md:text-xl text-lg font-semibold text-gray-800">Welcome back, <span class="capitalize"><?php if (isset($_SESSION['firstName'])) echo $_SESSION['firstName']; ?> 👋🏻</span></div>
+							<div class="w-full md:text-xl text-lg font-semibold text-gray-800">Welcome back, <span class="capitalize"><?php if (isset($admin['FName'])) echo $admin['FName']; ?> 👋🏻</span></div>
 							<div class="w-full text-sm text-gray-600 capitalize"><?php if (isset($_SESSION['type'])) echo $_SESSION['type']; ?></div>
 						</div>
 					</div>
 					<!-- notifications -->
 					<div class="w-1/3 mr-5 flex items-center justify-end text-gray-500">
 						<!-- notification icon -->
-						<button>
-							<div class="bg-gray-200 shadow rounded-md p-2">
+						<button id="dropdownNotificationBtn" data-dropdown-toggle="dropdownNotification">
+							<div class="bg-gray-200 shadow rounded-md p-2 hover:bg-gray-300">
 								<div class="relative">
 									<svg xmlns="http://www.w3.org/2000/svg" class="h-7" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
 										<path stroke-linecap="round" stroke-linejoin="round" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
@@ -179,10 +220,18 @@ require 'database/db_connection.php';
 				</div>
 				<!-- col 2: content -->
 				<div class="w-full h-[85%] overflow-y-auto overflow-x-hidden scrollbar">
-					<div class="w-full flex lg:flex-col flex-row flex-wrap ml-5 mt-5">
+					<div class="md:ml-5 mt-5">
+						<!-- Title -->
+						<p class="text-xl font-semibold capitalize w-full tracking-wide text-gray-700">Analytics dashboard</p>
+						<!-- date -->
+						<div class="text-sm tracking-wide text-gray-500 mt-1">
+							<?php echo date('F', mktime(0, 0, 0, date("m"))) . ' ' . date("d") . ', ' . date("l"); ?>
+						</div>
+					</div>
+					<!-- top cards -->
+					<div class="flex flex-col md:ml-5">
 						<!-- get numbers -->
 						<?php
-						$con = OpenCon();
 						$query = "SELECT username FROM student";
 						$result = mysqli_query($con, $query);
 						$numOfStudents = mysqli_num_rows($result);
@@ -200,69 +249,65 @@ require 'database/db_connection.php';
 						$numOfRequests = mysqli_num_rows($result);
 						?>
 						<!-- four numbers -->
-						<div id="fourCards" class="flex mt-4 mr-5">
-							<!-- students -->
-							<div class="w-fit mr-3">
-								<div class="flex items-center w-full bg-amber-50 px-5 py-4 shadow border border-amber-100 rounded-md">
-									<div>
-										<svg xmlns="http://www.w3.org/2000/svg" class="h-8 mr-3 pr-3 border-r border-gray-400" viewBox="0 0 20 20" fill="currentColor">
+						<div id="fourCards" class="flex flex-col md:flex-row mt-4 mr-2 w-full">
+							<div class="flex w-full">
+								<!-- students -->
+								<div class="mr-3 md:mb-0 mb-3 flex items-center w-full bg-amber-50 px-3 py-4 border border-gray-300 rounded-md">
+									<div class="bg-amber-300 rounded-full p-3 text-gray-800 mr-2">
+										<svg xmlns="http://www.w3.org/2000/svg" class="h-8" viewBox="0 0 20 20" fill="currentColor">
 											<path d="M10.394 2.08a1 1 0 00-.788 0l-7 3a1 1 0 000 1.84L5.25 8.051a.999.999 0 01.356-.257l4-1.714a1 1 0 11.788 1.838L7.667 9.088l1.94.831a1 1 0 00.787 0l7-3a1 1 0 000-1.838l-7-3zM3.31 9.397L5 10.12v4.102a8.969 8.969 0 00-1.05-.174 1 1 0 01-.89-.89 11.115 11.115 0 01.25-3.762zM9.3 16.573A9.026 9.026 0 007 14.935v-3.957l1.818.78a3 3 0 002.364 0l5.508-2.361a11.026 11.026 0 01.25 3.762 1 1 0 01-.89.89 8.968 8.968 0 00-5.35 2.524 1 1 0 01-1.4 0zM6 18a1 1 0 001-1v-2.065a8.935 8.935 0 00-2-.712V17a1 1 0 001 1z" />
 										</svg>
 									</div>
 									<div>
 										<h5 class="text-2xl font-semibold text-gray-800"><?php echo $numOfStudents ?></h5>
-										<p class="text-gray-700"><?php if ($numOfStudents > 1) echo "Registred students";
-																	elseif ($numOfStudents == 0) echo "There is no regirstred students yet";
-																	else echo "Registred student" ?></p>
+										<p class="text-gray-700 text-sm"><?php if ($numOfStudents > 1) echo "Registered students";
+																			elseif ($numOfStudents == 0) echo "There is no regirstred students yet";
+																			else echo "Registered student" ?></p>
 									</div>
 								</div>
-							</div>
-							<!-- instructors -->
-							<div class="w-fit mr-3">
-								<div class="flex items-center w-full bg-amber-50 px-5 py-4 shadow border border-amber-100 rounded-md">
-									<div>
-										<svg xmlns="http://www.w3.org/2000/svg" class="h-8 mr-3 pr-3 border-r border-gray-400" viewBox="0 0 20 20" fill="currentColor">
+								<!-- instructors -->
+								<div class="mr-3 md:mb-0 mb-3 flex items-center w-full bg-amber-50 px-3 py-4 border border-gray-300 rounded-md">
+									<div class="bg-amber-300 rounded-full p-3 text-gray-800 mr-2">
+										<svg xmlns="http://www.w3.org/2000/svg" class="h-8" viewBox="0 0 20 20" fill="currentColor">
 											<path d="M13 6a3 3 0 11-6 0 3 3 0 016 0zM18 8a2 2 0 11-4 0 2 2 0 014 0zM14 15a4 4 0 00-8 0v3h8v-3zM6 8a2 2 0 11-4 0 2 2 0 014 0zM16 18v-3a5.972 5.972 0 00-.75-2.906A3.005 3.005 0 0119 15v3h-3zM4.75 12.094A5.973 5.973 0 004 15v3H1v-3a3 3 0 013.75-2.906z" />
 										</svg>
 									</div>
 									<div>
 										<h5 class="text-2xl font-semibold text-gray-800"><?php echo $numOfInstructors ?></h5>
-										<p class="text-gray-700"><?php if ($numOfInstructors > 1) echo "Registred instructors";
-																	elseif ($numOfInstructors == 0) echo "There is no regirstred instructors yet";
-																	else echo "Instructor" ?></p>
+										<p class="text-gray-700 text-sm"><?php if ($numOfInstructors > 1) echo "Registered instructors";
+																			elseif ($numOfInstructors == 0) echo "There is no regirstred instructors yet";
+																			else echo "Instructor" ?></p>
 									</div>
 								</div>
 							</div>
-							<!-- courses -->
-							<div class="w-fit mr-3">
-								<div class="flex items-center w-full bg-amber-50 px-5 py-4 shadow border border-amber-100 rounded-md">
-									<div>
-										<svg xmlns="http://www.w3.org/2000/svg" class="h-8 mr-3 pr-3 border-r border-gray-400" viewBox="0 0 20 20" fill="currentColor">
+							<div class="flex w-full">
+								<!-- courses -->
+								<div class="mr-3 md:mb-0 mb-3 flex items-center w-full bg-amber-50 px-3 py-4 border border-gray-300 rounded-md">
+									<div class="bg-amber-300 rounded-full p-3 text-gray-800 mr-2">
+										<svg xmlns="http://www.w3.org/2000/svg" class="h-8" viewBox="0 0 20 20" fill="currentColor">
 											<path d="M9 4.804A7.968 7.968 0 005.5 4c-1.255 0-2.443.29-3.5.804v10A7.969 7.969 0 015.5 14c1.669 0 3.218.51 4.5 1.385A7.962 7.962 0 0114.5 14c1.255 0 2.443.29 3.5.804v-10A7.968 7.968 0 0014.5 4c-1.255 0-2.443.29-3.5.804V12a1 1 0 11-2 0V4.804z" />
 										</svg>
 									</div>
 									<div>
 										<h5 class="text-2xl font-semibold text-gray-800"><?php echo $numOfCourses ?></h5>
-										<p class="text-gray-700"><?php if ($numOfCourses > 1) echo "Published courses";
-																	elseif ($numOfCourses == 0) echo "There is no published courses yet";
-																	else echo "Published courses" ?></p>
+										<p class="text-gray-700 text-sm"><?php if ($numOfCourses > 1) echo "Published courses";
+																			elseif ($numOfCourses == 0) echo "There is no published courses yet";
+																			else echo "Published courses" ?></p>
 									</div>
 								</div>
-							</div>
-							<!-- requests -->
-							<div class="w-fit mr-3">
-								<div class="flex items-center w-full bg-amber-50 px-5 py-4 shadow border border-amber-100 rounded-md">
-									<div>
-										<svg xmlns="http://www.w3.org/2000/svg" class="h-8 mr-3 pr-3 border-r border-gray-400" viewBox="0 0 20 20" fill="currentColor">
+								<!-- requests -->
+								<div class="mr-3 md:mb-0 mb-3 flex items-center w-full bg-amber-50 px-3 py-4 border border-gray-300 rounded-md">
+									<div class="bg-amber-300 rounded-full p-3 text-gray-800 mr-2">
+										<svg xmlns="http://www.w3.org/2000/svg" class="h-8" viewBox="0 0 20 20" fill="currentColor">
 											<path d="M9 2a1 1 0 000 2h2a1 1 0 100-2H9z" />
 											<path fill-rule="evenodd" d="M4 5a2 2 0 012-2 3 3 0 003 3h2a3 3 0 003-3 2 2 0 012 2v11a2 2 0 01-2 2H6a2 2 0 01-2-2V5zm3 4a1 1 0 000 2h.01a1 1 0 100-2H7zm3 0a1 1 0 000 2h3a1 1 0 100-2h-3zm-3 4a1 1 0 100 2h.01a1 1 0 100-2H7zm3 0a1 1 0 100 2h3a1 1 0 100-2h-3z" clip-rule="evenodd" />
 										</svg>
 									</div>
 									<div>
 										<h5 class="text-2xl font-semibold text-gray-800"><?php echo $numOfRequests ?></h5>
-										<p class="text-gray-700"><?php if ($numOfRequests > 1) echo "New requests";
-																	elseif ($numOfRequests == 0) echo "You have been replied for all requests";
-																	else echo "New request" ?></p>
+										<p class="text-gray-700 text-sm"><?php if ($numOfRequests > 1) echo "New requests";
+																			elseif ($numOfRequests == 0) echo "You have been replied for all requests";
+																			else echo "New request" ?></p>
 									</div>
 								</div>
 							</div>
@@ -424,6 +469,7 @@ require 'database/db_connection.php';
 	</script>
 
 	<script src="js/analytics.js"></script>
+	<script src="https://unpkg.com/flowbite@1.4.7/dist/flowbite.js"></script>
 </body>
 
 </html>
