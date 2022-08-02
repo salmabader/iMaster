a21f62582a4b31b7b6fc308a8595515c54f6c2f9
salmabader <salma-bader-15@hotmail.com>
2022-08-02 07:09:42 +0300

    finish admin courses page

--- analytics.php
+++ analytics.php
@@ -118,7 +118,7 @@ if (isset($_POST['saveChangesBtn'])) {
 								</svg><span class="toHide">Issues</span></a>
 						</div>
 						<div class="hover:text-blue-800 duration-100 ease-in-out mb-5">
-							<a href="" class="pl-2 hover:border-l-[3px] duration-100 ease-in-out hover:border-l-amber-500 flex items-center"><svg xmlns="http://www.w3.org/2000/svg" class="h-5 inline mr-4 optionIcons" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="1.5">
+							<a href="adminCourses.php" class="pl-2 hover:border-l-[3px] duration-100 ease-in-out hover:border-l-amber-500 flex items-center"><svg xmlns="http://www.w3.org/2000/svg" class="h-5 inline mr-4 optionIcons" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="1.5">
 									<path stroke-linecap="round" stroke-linejoin="round" d="M4 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2V6zM14 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V6zM4 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2v-2zM14 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z" />
 								</svg><span class="toHide">Courses</span></a>
 						</div>
