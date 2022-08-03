301e8aaaf8b691cc80b613e589c178db51b9da03
salmabader <salma-bader-15@hotmail.com>
2022-08-03 21:49:37 +0300

    create requests page

--- requests.php
+++ requests.php
@@ -36,7 +36,7 @@ if (isset($_POST['saveChangesBtn'])) {
     <link rel="stylesheet" href="https://unpkg.com/flowbite@1.4.7/dist/flowbite.min.css" />
     <link rel="icon" href="images/icon.svg" type="image/x-icon">
     <script src="https://cdn.jsdelivr.net/npm/chart.js@3.8.2/dist/chart.min.js"></script>
-    <title>Analytics</title>
+    <title>Requests</title>
     <style>
         .scrollbar::-webkit-scrollbar {
             width: 10px;
@@ -107,18 +107,18 @@ if (isset($_POST['saveChangesBtn'])) {
                                     <path stroke-linecap="round" stroke-linejoin="round" d="M16 8v8m-4-5v5m-4-2v2m-2 4h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z" />
                                 </svg><span class="toHide">Analytics</span></a>
                         </div>
-                        <div class="hover:text-blue-800 duration-100 ease-in-out mb-5">
-                            <a href="requests.php" class="pl-2 hover:border-l-[3px] duration-100 ease-in-out hover:border-l-amber-500 flex items-center"><svg xmlns="http://www.w3.org/2000/svg" class="h-5 inline mr-4 optionIcons" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="1.5">
+                        <div class="text-blue-800 duration-100 ease-in-out mb-5">
+                            <a href="" class="pl-2 border-l-[3px] duration-100 ease-in-out border-l-amber-500 flex items-center"><svg xmlns="http://www.w3.org/2000/svg" class="h-5 inline mr-4 optionIcons" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="1.5">
                                     <path stroke-linecap="round" stroke-linejoin="round" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01" />
                                 </svg><span class="toHide">Requests</span></a>
                         </div>
                         <div class="hover:text-blue-800 duration-100 ease-in-out mb-5">
-                            <a href="" class="pl-2 hover:border-l-[3px] duration-100 ease-in-out hover:border-l-amber-500 flex items-center"><svg xmlns="http://www.w3.org/2000/svg" class="h-5 inline mr-4 optionIcons" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="1.5">
+                            <a href="issues.php" class="pl-2 hover:border-l-[3px] duration-100 ease-in-out hover:border-l-amber-500 flex items-center"><svg xmlns="http://www.w3.org/2000/svg" class="h-5 inline mr-4 optionIcons" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="1.5">
                                     <path stroke-linecap="round" stroke-linejoin="round" d="M19 20H5a2 2 0 01-2-2V6a2 2 0 012-2h10a2 2 0 012 2v1m2 13a2 2 0 01-2-2V7m2 13a2 2 0 002-2V9a2 2 0 00-2-2h-2m-4-3H9M7 16h6M7 8h6v4H7V8z" />
                                 </svg><span class="toHide">Issues</span></a>
                         </div>
-                        <div class="text-blue-800 duration-100 ease-in-out mb-5">
-                            <a href="" class="pl-2 border-l-[3px] duration-100 ease-in-out border-l-amber-500 flex items-center"><svg xmlns="http://www.w3.org/2000/svg" class="h-5 inline mr-4 optionIcons" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="1.5">
+                        <div class="hover:text-blue-800 duration-100 ease-in-out mb-5">
+                            <a href="adminCourses.php" class="pl-2 hover:border-l-[3px] duration-100 ease-in-out hover:border-l-amber-500 flex items-center"><svg xmlns="http://www.w3.org/2000/svg" class="h-5 inline mr-4 optionIcons" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="1.5">
                                     <path stroke-linecap="round" stroke-linejoin="round" d="M4 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2V6zM14 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V6zM4 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2v-2zM14 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z" />
                                 </svg><span class="toHide">Courses</span></a>
                         </div>
@@ -222,111 +222,7 @@ if (isset($_POST['saveChangesBtn'])) {
                 <div class="w-full h-[85%] overflow-y-auto overflow-x-hidden scrollbar">
                     <div class="md:ml-5 mt-5">
                         <!-- Title -->
-                        <p class="text-xl font-semibold capitalize w-full tracking-wide text-gray-700">All published courses</p>
-                    </div>
-                    <div id="myCoursesSection" class="md:ml-5 mt-5 flex md:flex-wrap gap-3 md:flex-row flex-col">
-                        <?php
-                        // complete query to get only accepted courses
-                        $query = "SELECT * FROM course";
-                        $result = mysqli_query($con, $query);
-                        if (mysqli_num_rows($result) > 0) { ?>
-                            <?php while ($course = mysqli_fetch_assoc($result)) {
-                            ?>
-                                <!-- card -->
-                                <div class="w-full md:w-1/5 bg-gray-50 rounded-md shadow-sm border border-gray-300 relative">
-                                    <!-- Image -->
-                                    <img class="h-40 object-cover rounded-t-md w-full" src="upload/<?php echo $course['image'] ?>">
-                                    <p class="absolute top-1 right-1
-                                    <?php if ($course['level'] == 0) echo "bg-yellow-100 text-yellow-800";
-                                    elseif ($course['level'] == 1) echo "bg-green-100 text-green-800";
-                                    else echo "bg-red-100 text-red-800";
-                                    ?> 
-                                    text-[10px] font-medium px-2.5 py-0.5 rounded">
-                                        <?php if ($course['level'] == 0) echo "Beginners";
-                                        elseif ($course['level'] == 1) echo "Intermediate";
-                                        else echo "Advanced"; ?>
-                                    </p>
-                                    <div class="p-2">
-                                        <!-- title -->
-                                        <p class="text-[10px] font-semibold pt-0 text-blue-700"><?php echo ucfirst($course['category']) ?></p>
-                                        <h2 class="font-bold text-lg mb-1 border-b border-gray-200 truncate"><?php echo ucfirst($course['title']) ?></h2>
-                                        <!-- instructor -->
-                                        <?php
-                                        // $sql = "SELECT * FROM course, instructor WHERE instructor_usename = username;";
-                                        $query = "SELECT * FROM instructor, course WHERE username = '" . $course['instructor_usename'] . "'";
-                                        $result2 = mysqli_query($con, $query);
-                                        $instructor = mysqli_fetch_assoc($result2);
-                                        ?>
-                                        <p class="text-sm text-gray-600 flex items-center"><svg xmlns="http://www.w3.org/2000/svg" class="h-3 inline mr-1" viewBox="0 0 20 20" fill="currentColor">
-                                                <path fill-rule="evenodd" d="M7 4a3 3 0 016 0v4a3 3 0 11-6 0V4zm4 10.93A7.001 7.001 0 0017 8a1 1 0 10-2 0A5 5 0 015 8a1 1 0 00-2 0 7.001 7.001 0 006 6.93V17H6a1 1 0 100 2h8a1 1 0 100-2h-3v-2.07z" clip-rule="evenodd" />
-                                            </svg><?php echo ucfirst($instructor['FName']) . " " . ucfirst($instructor['LName']) ?></p>
-                                    </div>
-                                    <!-- btns -->
-                                    <button class="py-1 bg-blue-200 rounded-b-md w-full flex justify-center text-md font-bold text-blue-700" data-modal-toggle="#show<?php echo ucfirst($course['courseID']) ?>">View</button>
-                                </div>
-                                <!-- end of card -->
-                                <!-- card model -->
-
-                                <!-- Main modal -->
-                                <div id="#show<?php echo ucfirst($course['courseID']) ?>" aria-hidden="true" tabindex="-1" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 w-full md:inset-0 h-modal md:h-full">
-                                    <div class="relative p-4 w-full max-w-2xl h-full md:h-auto">
-                                        <!-- Modal content -->
-                                        <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
-                                            <!-- Modal header -->
-                                            <div class="flex justify-between items-start p-4 rounded-t border-b dark:border-gray-600">
-                                                <h3 class="text-xl font-semibold  text-blue-700 dark:text-white">
-                                                    <?php echo ucfirst($course['title']) ?>
-                                                </h3>
-                                                <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-toggle="#show<?php echo ucfirst($course['courseID']) ?>">
-                                                    <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                                                        <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path>
-                                                    </svg>
-                                                    <span class="sr-only">Close modal</span>
-                                                </button>
-                                            </div>
-                                            <!-- Modal body -->
-                                            <div class="p-6 space-y-6 flex flex-col">
-                                                <div class="h-[20%] flex justify-between text-base font-bold leading-relaxed text-gray-500 dark:text-gray-400">
-                                                    <div><?php echo ucfirst($instructor['FName']) . " " . ucfirst($instructor['LName']);
-                                                            if ($course['collaborator'] != NULL) echo ", " . $course['collaborator'] ?></div>
-                                                    <div><?php echo ucfirst($course['category']) ?></div>
-                                                    <div class=" <?php if ($course['level'] == 0) echo " text-yellow-800";
-                                                                    elseif ($course['level'] == 1) echo " text-green-800";
-                                                                    else echo " text-red-800";
-                                                                    ?> 
-                                                            "><?php if ($course['level'] == 0) echo "Beginners";
-                                                                elseif ($course['level'] == 1) echo "Intermediate";
-                                                                else echo "Advanced"; ?></div>
-                                                </div>
-                                                <div class="text-base font-bold leading-relaxed text-gray-500 dark:text-gray-400">
-                                                    Description:
-                                                </div>
-                                                <div class="text-base leading-relaxed  text-gray-500 dark:text-gray-400">
-                                                    <?php echo ucfirst($course['description']) ?>
-                                                </div>
-                                                <div class="text-base font-bold leading-relaxed text-gray-500 dark:text-gray-400">
-                                                    Objectives:
-                                                </div>
-                                                <div class="text-base leading-relaxed  text-gray-500 dark:text-gray-400">
-                                                    <?php echo ucfirst($course['objectives']) ?>
-                                                </div>
-                                                <div class="text-base font-bold leading-relaxed text-gray-500 dark:text-gray-400">
-                                                    Requirements:
-                                                </div>
-                                                <div class="text-base leading-relaxed  text-gray-500 dark:text-gray-400">
-                                                    <?php echo ucfirst($course['requirements']) ?>
-                                                </div>
-                                            </div>
-
-                                        </div>
-                                    </div>
-                                </div>
-                                <!-- end of card model -->
-                            <?php }
-                        } else {
-                            ?>
-                            <p class="bg-gray-200 p-5 w-full rounded-lg">There is no courses yet</p>
-                        <?php } ?>
+                        <p class="text-xl font-semibold capitalize w-full tracking-wide text-gray-700">New instructors and courses</p>
                     </div>
                 </div>
             </div>
